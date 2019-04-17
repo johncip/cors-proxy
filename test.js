b06a3ae561094eb6d4ac10d46d5943981fd0588d
John Cipriano <johnmikecip@gmail.com>
2019-04-16 21:49:12 -0700

    try to tun test.js on heroku

---
+++ test.js
@@ -0,0 +1,17 @@
+const http = require('http')
+const logger = require('heroku-logger')
+
+const target = process.env.TARGET || 'http://localhost:9000'
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  })
+
+  res.end(`target: ${target}`)
+})
+
+const port = process.env.PORT || 8080
+logger.info('Starting server', { port: port })
+server.listen(port)
+
